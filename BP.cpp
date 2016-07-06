fcccd9481f142789fa9e6d022a2db4af378a532a
mwsht <ht@mws.site>
2016-07-06 09:08:08 +0800

    NEW

--- BP.cpp
+++ BP.cpp
@@ -1,35 +1,55 @@
 //#define _DEBUG_
+#define _TO_FILE_
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
 #include "BPMN.h"
 
-double E(mws::BP use,double *x,double *y);
+int test_num[]={2,3,3,2};
 
-int test_num[]={6,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,6};
+double f(double x);
 
-mws::BP test(20,test_num);
+mws::BP *test[8];
 
 int main()
 {
-	double x2[]={10,-259,-6,-7,10,-2};
-	double y2[]={1,0,1,1,1,0};
-	for (int i=0;i<100;i++)
+	double x_t[2],y_t[2];
+	srand(time(NULL));
+	
+	for (int i=1;i<2;i++)
 	{
-		test.tran(x2,y2);
-		printf("%f,",E(test,x2,y2));
-		//test.show(x2);
-	}
+		double E=0;
+		test_num[1]=i+3;
+		test_num[2]=i+3;
+		test[i]=new mws::BP(4,test_num,0.9,0.2,0.1);
+			x_t[1]=0;
+			y_t[1]=0;
+		for (int k=0;k<3000;k++)
+		{
+		for (double j=-4.0;j<=4.0;j=j+0.1)
+		{
+			x_t[0]=j;
+			y_t[0]=f(j);
+			test[i]->tran(x_t,y_t);
+		}
+		}
+		
+		printf("X,Y,N,E\n");
+		
+		for (double j=-3.975;j<=3.975;j=j+0.05)
+		{
+			x_t[0]=j;
+			test[i]->calculate_net(x_t);
+			E+=(mws::abs(test[i]->y[0]-f(j)))*(mws::abs(test[i]->y[0]-f(j)));
+			printf("%f,%f,%f,%f\n",j,f(j),test[i]->y[0],mws::abs(test[i]->y[0]-f(j)));
+		}
+		printf("%d,%f\n",i,sqrt(E/159.00));
+	}	
+	
 }
 
-double E(mws::BP use,double *x,double *y)
+double f(double x)
 {
-	double e=0;
-	use.calculate_net(x);
-	for (int j=0;j<6;j++)
-	{
-		e+=(use.y[j]-y[j])*(use.y[j]-y[j]);	
-	}
-	return e;
+	return ((1+x+x*x)*exp(-(x*x)/4))/3.0;
 }
